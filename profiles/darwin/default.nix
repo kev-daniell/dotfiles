8b3cfb8792c9d7898c894b5b08a7e4066383f2d3
Kevin Daniel <kevindkevdan@gmail.com>
2025-04-05 20:25:45 -0400

    refactor: migrate general configs into shared default.nix

--- profiles/darwin/default.nix
+++ profiles/darwin/default.nix
@@ -25,33 +25,6 @@
         };
       };
     };
-
-    sessionVariables = {
-      DIRENV_LOG_FORMAT = "";
-    };
-
-    stateVersion = "22.05";
-
-    packages = with pkgs; [
-      vim
-      gnupg
-
-      # Node 
-      nodejs
-      nodePackages.npm # globally install npm
-      yarn
-      nodePackages.prettier
-      nodePackages.nodemon
-      nodePackages.typescript-language-server
-
-      # Rust
-      rustc
-      cargo
-      rustfmt
-
-      python3
-      go
-    ];
   };
 
   programs = {
@@ -67,80 +40,6 @@
       shellAliases = {
         g = "git";
         h = "hx";
-      };
-    };
-
-    direnv = {
-      enable = true;
-      nix-direnv.enable = true;
-    };
-
-    git = {
-      enable = true;
-      userName = "%GIT_NAME";
-      userEmail = "%GIT_EMAIL";
-
-      extraConfig = {
-        init.defaultBranch = "main";
-        core = {
-          editor = "vim";
-          excludesFile = "~/.gitignore_global";
-        };
-        pull.rebase = true;
-        rebase.autoStash = true;
-        alias = {
-          gone = "!f() { git fetch --all --prune; git branch -vv | awk '/: gone]/{print $1}' | xargs git branch -D; }; f";
-          a = "add";
-          aa = "add --all";
-          ai = "add -i";
-          b = "branch";
-          ba = "branch -a";
-          bd = "branch -d";
-          bdd = "branch -D";
-          br = "branch -r";
-          bc = "rev-parse --abbrev-ref HEAD";
-          bu = "!git rev-parse --abbrev-ref --symbolic-full-name \"@{u}\"";
-          bs = "!git-branch-status";
-          c = "commit";
-          cm = "commit -m";
-          cl = "clone";
-          d = "diff";
-          f = "fetch";
-          fo = "fetch origin";
-          l = "log --oneline";
-          lg = "log --oneline --graph --decorate";
-          m = "merge";
-          ma = "merge --abort";
-          mc = "merge --continue";
-          ms = "merge --skip";
-          ch = "checkout";
-          chmst = "checkout master";
-          chmain = "checkout main";
-          chb = "checkout -b";
-          ph = "push";
-          phf = "push --force-with-lease";
-          pl = "pull";
-          pb = "pull --rebase";
-          plo = "pull origin";
-          pro = "pull --rebase origin";
-          plom = "pull origin master";
-          rb = "rebase";
-          rba = "rebase --abort";
-          rbc = "rebase --continue";
-          rbi = "rebase --interactive";
-          rbs = "rebase --skip";
-          uncommit = "reset HEAD~1";
-          s = "status";
-          st = "stash"; 
-          sc = "stash clear";
-          sd = "stash drop";
-          sl = "stash list";
-          sp = "stash pop";
-          ss = "stash save";
-          ssh = "stash show";
-          t = "tag";
-          td = "tag -d";
-        };
       };
     };
 
